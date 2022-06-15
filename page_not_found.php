6420a04fba3aa4b8c2cd2fc8fb79a8304d5ac0f5
supun-na97 <supunnanayakkara01@gmail.com>
2022-06-16 00:17:44 +0530

    Add 404 page not found section

---
+++ page_not_found.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 Not Found</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="assets/css/404-style.css"/>
+    <link rel="shortcut icon" href="images/logo.png">
+</head>
+
+<body>
+<div id="notfound">
+    <div class="notfound">
+        <img class="img-404" src="images/404.png" alt="supun nanayakkara">
+        <br><br>
+        <h2>404 - Page not found</h2>
+        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+        <a href="http://word.traditionalme.life">Go To Homepage</a>
+    </div>
+</div>
+
+</body>
+
+</html>
