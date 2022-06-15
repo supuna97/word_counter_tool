464b0344a4b3b61e8412d3452e85c63d0a1a9c11
supun-na97 <supunnanayakkara01@gmail.com>
2022-06-16 00:32:02 +0530

    Change base url

--- page_not_found.php
+++ page_not_found.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>404 Not Found</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="assets/css/404-style.css"/>
+    <link type="text/css" rel="stylesheet" href="css/404-style.css"/>
     <link rel="shortcut icon" href="images/logo.png">
 </head>
 
@@ -18,7 +18,7 @@
         <br><br>
         <h2>404 - Page not found</h2>
         <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-        <a href="http://word.traditionalme.life">Go To Homepage</a>
+        <a href="http://word-counter.traditionalme.life">Go To Homepage</a>
     </div>
 </div>
 
